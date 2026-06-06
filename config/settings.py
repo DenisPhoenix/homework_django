b733bda9927d01c48463701a1cf500943e6b48b9
name <email@gmail.com>
2026-06-06 08:48:37 +0300

    Добавление примеров переменных окружения в проект, настройка получения переменных окружения в настройках Django проекта

--- config/settings.py
+++ config/settings.py
@@ -4,13 +4,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-secret_key = os.getenv("SECRET_KEY")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = secret_key
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") else False
 
 ALLOWED_HOSTS = []
 
