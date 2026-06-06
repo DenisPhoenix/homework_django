9a5d6b150659a0d10c7c4b598634fd63eafe6a8f
name <email@gmail.com>
2026-06-06 22:49:50 +0300

    Изменение кастомной команды

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from django.db import connection
 
 from catalog.models import Category, Product
@@ -19,29 +20,8 @@ class Command(BaseCommand):
                 ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1;
             """)
 
-        # Добавление категории в БД
-        category, _ = Category.objects.get_or_create(name="Овощи")
-
-        # Список с продуктами
-        products = [
-            {"name": "Помидоры", "price": "25", "category": category},
-            {"name": "Огурцы", "price": "30", "category": category},
-            {"name": "Марковка", "price": "28", "category": category},
-            {"name": "Лук", "price": "15", "category": category},
-        ]
-
-        # Добавление продуктов в БД
-        for product_data in products:
-            product, created = Product.objects.get_or_create(**product_data)
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully added product: {product.name}"
-                    )
-                )
-            else:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Product already exists: {product.name}"
-                    )
-                )
+            call_command("loaddata", "catalog/fixtures/category_fixture.json")
+            call_command("loaddata", "catalog/fixtures/product_fixture.json")
+            self.stdout.write(
+                self.style.SUCCESS(f"Фикстура загружена в базу данных")
+            )
