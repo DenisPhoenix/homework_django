bebfb2bb3d5fd4331d32656635403dda50c8d4c4
name <email@gmail.com>
2026-06-12 17:23:43 +0300

    Добавление маршрутизации и контроллер для описания продукта

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
     if request.method == "GET":
@@ -13,3 +15,9 @@ def contacts(request):
         name = request.POST.get("name")
         return HttpResponse(f"Ответ получен {name}")
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
