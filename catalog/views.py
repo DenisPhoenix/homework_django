db3d8ec6e275e6ffc0424496d20974f2cd9a7c58
name <email@gmail.com>
2026-06-20 19:05:30 +0300

    Перевод контроллеров из FBV в CBV и добавление изменений маршрутам

--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
 
 from catalog.models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "catalog/home.html", context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        return HttpResponse(f"Ответ получен {name}")
-    return render(request, "catalog/contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
 
 
-def product_detail(request, pk):
-    product = Product.objects.get(id=pk)
-    context = {"product": product}
-    return render(request, "catalog/product_detail.html", context)
+class ContactTemplateView(TemplateView):
+    template_name = "catalog/contacts.html"
