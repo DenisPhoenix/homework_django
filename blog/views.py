846ecefa7586dd661901ffd602bb18b36f9570f7
name <email@gmail.com>
2026-06-21 18:16:15 +0300

    Переопределение методов и добавление доп. настроек классам контроллерам

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,24 @@
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-from models import Blog
+from django.db.models import F
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from .models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog/blog_list.html"
+    context_object_name = "blogs"
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(is_publication=True)
+        return queryset
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = "blog/blog_confirm_delete.html"
+    success_url = reverse_lazy("blog:blog_list")
 
 
 class BlogCreateView(CreateView):
@@ -11,10 +30,8 @@ class BlogCreateView(CreateView):
         "is_publication",
         "counter_view",
     ]
-
-
-class BlogListView(ListView):
-    model = Blog
+    template_name = "blog/blog_form.html"
+    success_url = reverse_lazy("blog:blog_list")
 
 
 class BlogUpdateView(UpdateView):
@@ -26,7 +43,19 @@ class BlogUpdateView(UpdateView):
         "is_publication",
         "counter_view",
     ]
+    template_name = "blog/blog_form.html"
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", kwargs={"pk": self.object.pk})
 
 
-class BlogDeleteView(DeleteView):
+class BlogDetailView(DetailView):
     model = Blog
+    template_name = "blog/blog_detail.html"
+    context_object_name = "blog"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.counter_view = F("counter_view") + 1
+        obj.save(update_fields=["counter_view"])
+        return obj
