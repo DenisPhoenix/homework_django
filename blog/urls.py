3ce55315f5e75f70f873b51277fcb8698ac39c24
name <email@gmail.com>
2026-06-21 18:18:12 +0300

    Добавление маршрутизации классам с использованием CBV подхода

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,23 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", views.BlogListView.as_view(), name="home"),
+    path("home/", views.BlogListView.as_view(), name="blog_list"),
+    path(
+        "blog_create/new/", views.BlogCreateView.as_view(), name="blog_create"
+    ),
+    path(
+        "blog_detail/<int:pk>/",
+        views.BlogDetailView.as_view(),
+        name="blog_detail",
+    ),
+    path(
+        "blog_delete/<int:pk>/",
+        views.BlogDeleteView.as_view(),
+        name="blog_delete",
+    ),
+    path(
+        "blog_edit/<int:pk>/",
+        views.BlogUpdateView.as_view(),
+        name="blog_edit",
+    ),
 ]
