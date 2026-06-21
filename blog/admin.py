a4733b491a695adc0db328d136cc37465d991610
name <email@gmail.com>
2026-06-21 18:21:11 +0300

    Добавление отображения модели blog в админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "create_at",
+        "is_publication",
+        "counter_view",
+    )
+    list_filter = ("title",)
+    search_fields = ("title",)
