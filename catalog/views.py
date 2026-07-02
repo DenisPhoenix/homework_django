4a32420b0ccc7f18fca8bf2a981057787cc8a2d4
name <email@gmail.com>
2026-07-02 11:22:15 +0300

    Создание котроллеров с CRUD-операциями, включение в представления форм

--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,41 @@
-from django.views.generic import DetailView, ListView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
 from catalog.models import Product
+
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = "catalog/home.html"
+    template_name = "catalog/product/product_list.html"
     context_object_name = "products"
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = "catalog/product_detail.html"
+    template_name = "catalog/product/product_detail.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product/product_form.html"
+    success_url = reverse_lazy("category:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product/product_form.html"
+    success_url = reverse_lazy("category:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product/product_confirm_delete.html"
+    success_url = reverse_lazy("category:product_list")
 
 
 class ContactTemplateView(TemplateView):
