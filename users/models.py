f7ba2e03b2587e7de20ff92742dec2d1d054708e
name <email@gmail.com>
2026-07-08 10:39:56 +0300

    Создание модели для пользователя с дополнительными полями, создание миграций и применение их, настройка модели используемой при аутентификации

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.CharField(
+        max_length=50, unique=True, verbose_name="Электронная почта"
+    )
+
+    phone_number = models.CharField(
+        max_length=15, blank=True, null=True, verbose_name="Номер телефона"
+    )
+    country = models.CharField(
+        max_length=50, blank=True, null=True, verbose_name="Город проживания"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar", blank=True, null=True, verbose_name="Аватар"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username",)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
