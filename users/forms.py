5175acdc10fc97e4c866f4a594154e951544c313
name <email@gmail.com>
2026-07-08 11:22:55 +0300

    Создание кастомной формы для регистрации пользователей, подключение класса миксин для стилизации полей формы

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import BooleanField
+
+from .models import User
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs.update({"class": "form-check-input"})
+            else:
+                field.widget.attrs.update({"class": "form-control"})
+
+
+class CustomUserCreationForm(StyleFormMixin, UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email", "password1", "password2")
