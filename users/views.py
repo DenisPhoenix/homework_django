48f7269e8c0386b014937f2cef91d9407b19cb36
name <email@gmail.com>
2026-07-08 11:25:34 +0300

    Создание предстовления для регистрациии, настройка маршрутизации

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CustomUserCreationForm
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "users/registration/register.html"
+    success_url = reverse_lazy("product_list")
